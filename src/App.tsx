d056261a3c5e00c04d112a4012f2e3a227b43a7a
MuhammadYusufAndrika <abdrika00748@gmail.com>
2025-02-27 00:07:00 +0700

    update global

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
 import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
